cb4b4c35f89f702a41fab55bafbf745f064e1db9
ardillaCHIKI <svegazos@student.universidadunie.com>
2025-03-19 18:10:30 +0100

    Enhance Bicicleta and Coche classes with additional attributes and string representation methods

--- subclases/bicicleta.py
+++ subclases/bicicleta.py
@@ -1,6 +1,17 @@
 from subclases import Vehiculo
 
 class Bicicleta(Vehiculo):
-    def __init__(self, marca, modelo, tipo):
-        super().__init__(marca, modelo)
+    def __init__(self, color, ruedas, marca, modelo, tipo):
+        super().__init__(color, ruedas)
         self.tipo = tipo
+        self.marca = marca
+        self.modelo = modelo
+
+    def __str__(self):
+        return f"Color: {self.color}, Ruedas: {self.ruedas}, Marca: {self.marca}, Modelo: {self.modelo}, Tipo: {self.tipo}"
+    
+    def catalogar(vehiculos):
+        for vehiculo in vehiculos:
+            print(f"{vehiculo.__class__.__name__} : {vehiculo}")
+
+
--- subclases/coche.py
+++ subclases/coche.py
@@ -6,6 +6,9 @@ class Coche(Vehiculo):
         self.velocidad = velocidad
         self.cilindrada = cilindrada
 
+    def __str__(self):
+        return f"Color: {self.color}, Velocidad: {self.velocidad} km/h, Ruedas: {self.ruedas}, Cilindrada: {self.cilindrada}"
+
     def catalogar(vehiculos):
         for vehiculo in vehiculos:
             print(f"{vehiculo.__class__.__name__} : {vehiculo}")
