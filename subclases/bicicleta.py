bedc337d5a2ee69a55ba349dc5d176ee67f7795a
ardillaCHIKI <svegazos@student.universidadunie.com>
2025-03-19 16:44:48 +0100

    mini cambio en bicicleta

--- subclases/bicicleta.py
+++ subclases/bicicleta.py
@@ -1,4 +1,4 @@
-import Vehiculo
+from subclases import Vehiculo
 
 class Bicicleta(Vehiculo):
     def __init__(self, marca, modelo, tipo):
--- subclases/coche.py
+++ subclases/coche.py
@@ -1,6 +1,5 @@
 from subclases import Vehiculo
 
-
 class Coche(Vehiculo):
     def __init__(self, marca, modelo, puertas):
         super().__init__(marca, modelo)
