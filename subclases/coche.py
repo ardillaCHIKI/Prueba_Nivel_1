0d59cfc5f6b083c702aa68ba164ab8a7fbde1bb7
ardillaCHIKI <svegazos@student.universidadunie.com>
2025-03-19 17:39:07 +0100

    cambio de variables en coche

--- subclases/coche.py
+++ subclases/coche.py
@@ -1,6 +1,7 @@
 from subclases import Vehiculo
 
 class Coche(Vehiculo):
-    def __init__(self, marca, modelo, puertas):
-        super().__init__(marca, modelo)
-        self.puertas = puertas
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
